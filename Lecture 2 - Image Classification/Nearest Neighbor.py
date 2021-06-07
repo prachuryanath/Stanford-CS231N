82787ced65608d58f56b5a9b59ef47f4365e2b9c
Prachurya Nath <prachuryanath00@gmail.com>
2021-06-07 19:09:35 +0530

    updated

--- Lecture 2 - Image Classification/Nearest Neighbor.py
+++ Lecture 2 - Image Classification/Nearest Neighbor.py
@@ -31,4 +31,16 @@ class NearestNeighbor:
 
         return Ypred
 
+Xtr, Ytr, Xte, Yte = load_CIFAR10('data/cifar10/') # a magic function we provide
+# flatten out all images to be one-dimensional
+Xtr_rows = Xtr.reshape(Xtr.shape[0], 32 * 32 * 3) # Xtr_rows becomes 50000 x 3072
+Xte_rows = Xte.reshape(Xte.shape[0], 32 * 32 * 3) # Xte_rows becomes 10000 x 3072
+
+nn = NearestNeighbor() # create a Nearest Neighbor classifier class
+nn.train(Xtr_rows, Ytr) # train the classifier on the training images and labels
+Yte_predict = nn.predict(Xte_rows) # predict labels on the test images
+# and now print the classification accuracy, which is the average number
+# of examples that are correctly predicted (i.e. label matches)
+print ('accuracy: %f' % ( np.mean(Yte_predict == Yte) ))
+
 # With N examples, Train O(1)   Prediction O(n)
