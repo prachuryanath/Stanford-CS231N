ab06bf471b9ffb61a98a93a2125af0636173e20f
Prachurya Nath <prachuryanath00@gmail.com>
2021-07-26 20:18:41 +0530

    Update gradient descent.py

--- Lecture 3 - Loss functions & Optimization/gradient descent.py
+++ Lecture 3 - Loss functions & Optimization/gradient descent.py
@@ -9,7 +9,9 @@ while True:
   weights_grad = evaluate_gradient(loss_fun, data, weights)
   weights += - step_size * weights_grad # perform parameter update
 
+
 # Vanilla Minibatch Gradient Descent
+
 
 while True:
   data_batch = sample_training_data(data, 256) # sample 256 examples
